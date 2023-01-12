e945dd27ac1e67f38ba2538b8e6b68e6053a76b9
zy445566 <zyct@vip.qq.com>
2023-01-12 21:47:34 +0800

    feat: 添加引用

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,11 @@
+import {VaasServerConfigKey} from './server/lib/decorator'
+
 export function entryClassMixins(baseClass: any, classItemList: any[]) {
     for (const classItem of classItemList) {
       const funcNameList = Object.getOwnPropertyNames(classItem.prototype)
       for(const funcName of funcNameList) {
         if(baseClass.prototype[funcName]) {
-          if(funcName ==='__appConfig') {
+          if(funcName === VaasServerConfigKey) {
             baseClass.prototype[funcName] = new Map([
               ...baseClass.prototype[funcName].entries(),
               ...classItem.prototype[funcName].entries()
