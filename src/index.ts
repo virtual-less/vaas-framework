1735d97c9057c0ba85e81d8cb4b4fbdccfe6927a
zy445566 <zyct@vip.qq.com>
2023-01-12 21:34:09 +0800

    feat: add util

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export  * as Decorator from './server/lib/decorator'
 
 export {rpcInvote} from './server/lib/rpc';
 export {VaasServer} from './server/index'
+
+export * as util from './util'
---
+++ src/util.ts
@@ -0,0 +1,21 @@
+export function entryClassMixins(baseClass: any, classItemList: any[]) {
+    for (const classItem of classItemList) {
+      const funcNameList = Object.getOwnPropertyNames(classItem.prototype)
+      for(const funcName of funcNameList) {
+        if(baseClass.prototype[funcName]) {
+          if(funcName ==='__appConfig') {
+            baseClass.prototype[funcName] = new Map([
+              ...baseClass.prototype[funcName].entries(),
+              ...classItem.prototype[funcName].entries()
+            ])
+            continue
+          }
+          if(funcName ==='constructor') {
+            continue
+          }
+          throw new Error('该方法已存在，不能进行entryClassMixins')
+        }
+        baseClass.prototype[funcName] = classItem.prototype[funcName]
+      }
+    }
+}
