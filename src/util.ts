0f8f4c7492b31788b2e0c9d7b1d3d60f2c5a747c
zy445566 <zyct@vip.qq.com>
2024-04-01 12:03:00 +0800

    feat: 方法废弃

--- src/util.ts
+++ src/util.ts
@@ -1,34 +1,4 @@
-import { getVaasServerMap } from './server/lib/decorator'
 import { type VaasConfig } from './types/server'
-
-export function entryClassMixins (baseClass: any, classItemList: any[]) {
-  for (const classItem of classItemList) {
-    const funcNameList = Object.getOwnPropertyNames(classItem.prototype)
-    for (const funcName of funcNameList) {
-      if (baseClass.prototype[funcName]) {
-        const baseClassMap = getVaasServerMap(baseClass.prototype)
-        if (baseClassMap.size > 0) {
-          const classItemMap = getVaasServerMap(classItem.prototype)
-          baseClass.prototype[funcName] = new Map([
-            ...baseClassMap.entries(),
-            ...classItemMap.entries()
-          ])
-          continue
-        }
-        if (funcName === 'constructor') {
-          baseClass.prototype[funcName] = (...args) => {
-            classItem.prototype[funcName](...args)
-            return baseClass.prototype[funcName](...args)
-          }
-          continue
-        }
-        throw new Error('该方法已存在，不能进行entryClassMixins')
-      }
-      baseClass.prototype[funcName] = classItem.prototype[funcName]
-    }
-  }
-  return baseClass
-}
 
 // 仅用来校验vaas.config.js配置的类型检查
 export function validVaasConfig (config: VaasConfig): VaasConfig {
